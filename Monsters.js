df518b38d0afd00692f5cfee729b39422e841300
amiekisiak <117371691+amiekisiak@users.noreply.github.com>
2023-05-02 19:33:09 +0100

    added background picture

--- Monsters.js
+++ Monsters.js
@@ -16,19 +16,20 @@ window.addEventListener('load', function () {
             this.collisionRadius = 30;
 
         }
-        draw(context){
-        context.beginPath()
-        context.arc(this.collisonX, this.collisionY,this.collisionRadius,100,100,50,0, Math.PI * 2);
-        context.save(); //wrapping it so not every object is semi-transparent, it is a snapshot of the current canvas state
-        context.globalAlpha = 0.5; // because it is wrapped between save and restore affects on this speacific action
-        context.fill();
-        context.restore(); // restoring to the previous point
-        context.stroke(); // this won't be affected by reduced opacity
+        draw(context) {
+            context.beginPath()
+            context.arc(this.collisonX, this.collisionY, this.collisionRadius, 100, 100, 50, 0, Math.PI * 2);
+            context.save(); //wrapping it so not every object is semi-transparent, it is a snapshot of the current canvas state
+            context.globalAlpha = 0.5; // because it is wrapped between save and restore affects on this speacific action
+            context.fill();
+            context.restore(); // restoring to the previous point
+            context.stroke(); // this won't be affected by reduced opacity
         }
-        render(context){
-        this.player.draw(context);
+        update() {
+            this.collisionX = this.game.mouse.x;
+            this.collisionY = this.game.mouse.y;
+        }
 
-        }
     }
 
 
@@ -37,29 +38,42 @@ window.addEventListener('load', function () {
             this.canvas = canvas;
             this.width = this.canvas.width;
             this.height = this.canvas.height;
-            this.palyer = new Player (this);
+            this.palyer = new Player(this);
             this.mouse = {
-               x: this.width * 0.5,
-               y: this.height * 0.5,
-               pressed: false
+                x: this.width * 0.5,
+                y: this.height * 0.5,
+                pressed: false
 
             }
 
-//event listeners
+            //event listeners
 
 
-    canvas.addEventListener ('mousedown', (e) =>{
-        this.mouse.x = e.offesetX;
-        this.mouse.y = e.offsetY;
-    
+            canvas.addEventListener('mousedown', (e) => {
+                this.mouse.x = e.offesetX;
+                this.mouse.y = e.offsetY;
+                this.mouse.pressed = true;
 
-    });
-}
 
-render(context){
-this.player.draw(context);
-}
-}
+            });
+            canvas.addEventListener('mouseup', (e) => {
+                this.mouse.x = e.offesetX;
+                this.mouse.y = e.offsetY;
+                this.mouse.pressed = false;
+            });
+            canvas.addEventListener('mousemove', (e) => {
+                this.mouse.x = e.offesetX;
+                this.mouse.y = e.offsetY;
+                this.mouse.pressed = false;
+            });
+
+        }
+
+        render(context) {
+            this.player.draw(context);
+            this.player.update();
+        }
+    }
 
     const game = new Game(canvas);
     game.render(ctx);
@@ -67,8 +81,14 @@ this.player.draw(context);
 
 
     function animate() {
+        ctx.clearRect(0,0, canvas.width, canvas.height);
+        game.render(ctx);
+        requestAnimationFrame(animate);
+
 
     }
+    animate();
 
-   
-})
+});
+
+
